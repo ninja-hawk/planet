d8a05b51b8805f401b68352ff89e45beca2c1ac2
ninja-hawk <saule.faucon@gmail.com>
2024-06-08 00:22:08 +0900

    update: revive seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,8 +2,8 @@
 
 
 # Load individual seed files
-# Code.eval_file("priv/repo/seeds/user.exs")
-# Code.eval_file("priv/repo/seeds/social_issue.exs")
-# Code.eval_file("priv/repo/seeds/asset.exs")
+Code.eval_file("priv/repo/seeds/user.exs")
+Code.eval_file("priv/repo/seeds/social_issue.exs")
+Code.eval_file("priv/repo/seeds/asset.exs")
 Code.eval_file("priv/repo/seeds/solution.exs")
 # Add more files as needed
