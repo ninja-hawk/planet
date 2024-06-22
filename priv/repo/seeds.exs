876025dedb07ea15cf522e23ff5bf43c17886549
ninja-hawk <saule.faucon@gmail.com>
2024-06-22 17:47:08 +0900

    add: solution assets seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -6,4 +6,5 @@ Code.eval_file("priv/repo/seeds/user.exs")
 Code.eval_file("priv/repo/seeds/social_issue.exs")
 Code.eval_file("priv/repo/seeds/asset.exs")
 Code.eval_file("priv/repo/seeds/solution.exs")
+Code.eval_file("priv/repo/seeds/solution_asset.exs")
 # Add more files as needed
