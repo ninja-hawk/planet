b7961db5702107f52772e59e18e5dc120c890ffc
ninja-hawk <saule.faucon@gmail.com>
2024-06-24 16:28:26 +0900

    add: post form

--- lib/planet_web/controllers/check_asset_controller.ex
+++ lib/planet_web/controllers/check_asset_controller.ex
@@ -6,12 +6,17 @@ defmodule PlanetWeb.CheckAssetController do
   def index(conn, _params) do
     assets = Cosmos.list_assets()
     social_issues = Cosmos.list_social_issues()
-    render(conn, :index, assets: assets, social_issues: social_issues)
+    action = "/check_asset/check"
+    render(conn, :index, assets: assets, social_issues: social_issues, action: action)
   end
 
-  def show(conn, _params) do
+  def show(conn, %{"social_issue_id" => social_issue_id, "assets" => assets, "num_of_youth_pop" => num_of_youth_pop}) do
     conn
-    |> put_flash(:info, "Asset created successfully.")
-    render(conn, :show)
+    |> put_flash(:info, "Correct!")
+    |> render(:show, social_issue_id: social_issue_id, assets: assets, num_of_youth_pop: num_of_youth_pop)
+  end
+
+  def check(conn, %{"social_issue_id" => social_issue_id, "assets" => assets, "num_of_youth_pop" => num_of_youth_pop}) do
+    render(conn, :show, social_issue_id: social_issue_id, assets: assets, num_of_youth_pop: num_of_youth_pop)
   end
 end
