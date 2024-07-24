5da368ad7b2cc89e08de4ce2aa51203c0a097441
ninja-hawk <saule.faucon@gmail.com>
2024-07-24 17:30:03 +0900

    add: check asset checkbox and form

--- lib/planet_web/controllers/check_asset_controller.ex
+++ lib/planet_web/controllers/check_asset_controller.ex
@@ -11,12 +11,18 @@ defmodule PlanetWeb.CheckAssetController do
   end
 
   def show(conn, %{"social_issue_id" => social_issue_id, "assets" => assets, "num_of_youth_pop" => num_of_youth_pop}) do
+    social_issue = Cosmos.get_social_issue!(social_issue_id)
     conn
     |> put_flash(:info, "Correct!")
-    |> render(:show, social_issue_id: social_issue_id, assets: assets, num_of_youth_pop: num_of_youth_pop)
+    |> render(:show, social_issue_id: social_issue_id, social_issue: social_issue, assets: assets, num_of_youth_pop: num_of_youth_pop)
   end
 
   def check(conn, %{"social_issue_id" => social_issue_id, "assets" => assets, "num_of_youth_pop" => num_of_youth_pop}) do
-    render(conn, :show, social_issue_id: social_issue_id, assets: assets, num_of_youth_pop: num_of_youth_pop)
+    social_issue = Cosmos.get_social_issue!(social_issue_id)
+    Enum.each(social_issue.solutions, fn solution ->
+      IO.inspect(solution)
+    end)
+    IO.puts("Received params: #{social_issue.situation}")
+    render(conn, :show, social_issue_id: social_issue_id, social_issue: social_issue, assets: assets, num_of_youth_pop: num_of_youth_pop)
   end
 end
