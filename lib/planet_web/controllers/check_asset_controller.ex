e402de50ecc64a17a70cf32f379ea1095e83129c
ninja-hawk <saule.faucon@gmail.com>
2024-07-24 18:16:48 +0900

    update: use only 2 assets

--- lib/planet_web/controllers/check_asset_controller.ex
+++ lib/planet_web/controllers/check_asset_controller.ex
@@ -10,19 +10,19 @@ defmodule PlanetWeb.CheckAssetController do
     render(conn, :index, assets: assets, social_issues: social_issues, action: action)
   end
 
-  def show(conn, %{"social_issue_id" => social_issue_id, "assets" => assets, "num_of_youth_pop" => num_of_youth_pop}) do
+  def show(conn, %{"social_issue_id" => social_issue_id, "asset_1" => asset_1, "asset_2" => asset_2, "num_of_youth_pop" => num_of_youth_pop}) do
     social_issue = Cosmos.get_social_issue!(social_issue_id)
     conn
     |> put_flash(:info, "Correct!")
-    |> render(:show, social_issue_id: social_issue_id, social_issue: social_issue, assets: assets, num_of_youth_pop: num_of_youth_pop)
+    |> render(:show, social_issue_id: social_issue_id, social_issue: social_issue, asset_1: asset_1, asset_2: asset_2, num_of_youth_pop: num_of_youth_pop)
   end
 
-  def check(conn, %{"social_issue_id" => social_issue_id, "assets" => assets, "num_of_youth_pop" => num_of_youth_pop}) do
+  def check(conn, %{"social_issue_id" => social_issue_id, "asset_1" => asset_1, "asset_2" => asset_2, "num_of_youth_pop" => num_of_youth_pop}) do
     social_issue = Cosmos.get_social_issue!(social_issue_id)
     Enum.each(social_issue.solutions, fn solution ->
       IO.inspect(solution)
     end)
     IO.puts("Received params: #{social_issue.situation}")
-    render(conn, :show, social_issue_id: social_issue_id, social_issue: social_issue, assets: assets, num_of_youth_pop: num_of_youth_pop)
+    render(conn, :show, social_issue_id: social_issue_id, social_issue: social_issue, asset_1: asset_1, asset_2: asset_2, num_of_youth_pop: num_of_youth_pop)
   end
 end
